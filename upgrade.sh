3576a72934f4c4feb6035b7a01a7afe802e74495
Allen K. Lair <allenklair@gmail.com>
2019-01-07 19:22:06 -0500

    Add simple zenity progress dialog for upgrade.

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Update the sources and build
+zenity --progress --text="Upgrading RoscoPi. Please wait..." --percentage=0 --auto-close
 cd /home/pi/RoscoPi
+zenity --progress --text="Upgrading RoscoPi. Please wait..." --percentage=33 --auto-close
 git pull
+zenity --progress --text="Upgrading RoscoPi. Please wait..." --percentage=66 --auto-close
 make
+zenity --progress --text="Upgrading RoscoPi. Please wait..." --percentage=100 --auto-close
+bin/RoscoPi
