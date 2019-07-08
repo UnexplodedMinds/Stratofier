543d760f1a5355d9de83c1130ec621fe8e0ef46e
Allen K. Lair <allenklair@gmail.com>
2019-07-07 20:43:26 -0400

    Change upgrade window title to Stratofier.

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Update the sources and build
-lxterminal --geometry=57x30 -t "UPGRADING ROSCOPI" -e /home/pi/Stratofier/upgrade2.sh
+lxterminal --geometry=57x30 -t "UPGRADING STRATOFIER" -e /home/pi/Stratofier/upgrade2.sh
